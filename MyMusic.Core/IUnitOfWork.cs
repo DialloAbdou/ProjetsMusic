6f0dbed8ab844432850ef5f504a88bb0cea20dae
DialloAbdou <abdoudiallo3826@yahoo.fr>
2020-12-15 19:18:43 +0100

    Creation squelette WebAPI & CoreEntityLayer

---
+++ MyMusic.Core/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using MyMusic.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Core
+{
+    public interface IUnitOfWork
+    {
+        IArtistRepository Artist { get; }
+        IMusicRepository Music { get; }
+        Task<int> CommitAsync();
+    }
+}
---
+++ MyMusic.Core/Models/Music.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyMusic.Core.Models
+{
+    public class Music
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
