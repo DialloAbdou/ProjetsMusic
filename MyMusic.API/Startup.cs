4f223a134b7ec277092faf5678b90301c0772cf9
DialloAbdou <abdoudiallo3826@yahoo.fr>
2020-12-29 16:51:45 +0100

    Creation Swagger

--- MyMusic.API/Startup.cs
+++ MyMusic.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using MongoDB.Driver;
 using MyMusic.Core;
 using MyMusic.Core.Repositories;
@@ -14,6 +15,7 @@ using MyMusic.Core.Services;
 using MyMusic.Data;
 using MyMusic.Data.MongoDB.Repository;
 using MyMusic.Data.MongoDB.Setting;
+using MyMusic.Services.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,13 +47,26 @@ namespace MyMusic.API
                     options.ConnectionString = Configuration.GetValue<string>("MongoDB:ConnectionString");
                     options.DataBase = Configuration.GetValue<string>("MongoDB:Database");
                 });
+
+            //===Connexion De MongoDb=====
             services.AddSingleton<IMongoClient, MongoClient>(_=> new MongoClient(Configuration.GetValue<string>("MongoDB:ConnectionString")));
 
             // Injection Repo
             services.AddScoped<IComposerRepository, ComposerRepository>();
+            services.AddTransient<IDatabaseSettings, DataBaseSetting>();
 
             // les Differents Services 
-           
+            services.AddTransient<IMusicServices, MusicService>();
+            services.AddTransient<IArtistService, ArtistService>();
+            services.AddTransient<IComposerService, ComposerService>();
+
+            //Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                { Title = "Put title here", Description = "DotNet Core Api 3 - with swagger" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +86,12 @@ namespace MyMusic.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Music V1");
             });
         }
     }
