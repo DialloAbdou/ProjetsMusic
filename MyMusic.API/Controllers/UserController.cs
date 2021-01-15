b8d234a15e1ff98d2646b2b2ea95c63e5d41faca
DialloAbdou <abdoudiallo3826@yahoo.fr>
2021-01-15 22:00:55 +0100

    Ajout Controller User

--- MyMusic.API/Controllers/UserController.cs
+++ MyMusic.API/Controllers/UserController.cs
@@ -1,11 +1,20 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MyMusic.API.Ressources;
+using MyMusic.API.Validation;
+using MyMusic.Core.Models;
 using MyMusic.Core.Services;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+
 
 namespace MyMusic.API.Controllers
 {
@@ -18,11 +27,82 @@ namespace MyMusic.API.Controllers
         private readonly Microsoft.Extensions.Configuration.IConfiguration _config;
 
         public UserController(IUserServices userServices, IMapper mapper,
-            Microsoft.Extensions.Configuration.IConfiguration config)
+           Microsoft.Extensions.Configuration.IConfiguration config)
         {
             _userServices = userServices;
             _mapper = mapper;
             _config = config;
         }
+
+        [HttpPost("")]
+
+        public async Task<ActionResult> Authenticate( UserRessource userRessource)
+        {
+            var user= await _userServices.Authenticate(userRessource.Username, userRessource.Password);
+
+            if (user == null) return BadRequest(new { message = "username or password is incorrect" });
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.GetValue<string>("AppSettings:Secret"));
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                   new Claim(ClaimTypes.Name, user.Id.ToString())
+               }),
+                   Expires = DateTime.UtcNow.AddDays(7),
+                   SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return Ok(new
+            {
+                user.Id,
+                user.Username,
+                user.FirstName,
+                user.LastName,
+                Token = tokenString
+            });
+
+
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRessource userResource)
+        {
+            // validation
+            var validation = new SaveUSerRessourceValidator();
+            var validationResult = await validation.ValidateAsync(userResource);
+            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+            var user = _mapper.Map<UserRessource, User>(userResource);
+            // mappage
+            var userSave = await _userServices.Create(user, userResource.Password);
+            //send tocken 
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.GetValue<string>("AppSettings:Secret"));
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                 {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                 }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return Ok(new
+            {
+                Id = user.Id,
+                Username = user.Username,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = tokenString
+            });
+
+        }
+
+
     }
 }
